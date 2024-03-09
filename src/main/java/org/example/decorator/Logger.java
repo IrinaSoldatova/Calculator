78289ba17f23bdb2a693730343dce5e07e5dff5b
Irina Soldatova <soldatovaik@yandex.ru>
2024-03-09 23:55:54 +0300

    add date of file logger

--- src/main/java/org/example/decorator/Logger.java
+++ src/main/java/org/example/decorator/Logger.java
@@ -2,14 +2,18 @@ package org.example.decorator;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Logger {
     private static final String FILE_PATH = "log.txt";
 
     public static void log(String message) {
         try {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String formattedDateTime = dateFormat.format(new Date());
             FileWriter writer = new FileWriter(FILE_PATH, true);
-            writer.write(message + "\n");
+            writer.write(formattedDateTime + " - " + message + "\n");
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
